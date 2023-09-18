7f1227e8d3268e2323f46deb3366840a8611d88d
John the Fox <joao.chivite@gmail.com>
2023-09-18 13:06:57 -0300

    Update main.c

--- estrutura de dados/main.c
+++ estrutura de dados/main.c
@@ -4,10 +4,16 @@
 
 int main(int argc, char *argv[]) {
   Tree *my_tree = create_tree();
-  inserir(my_tree, 7);
-  inserir(my_tree, 5);
-  inserir(my_tree, 9);
-  inserir(my_tree, 3);
+  insert(my_tree, 7);
+  insert(my_tree, 5);
+  insert(my_tree, 9);
+  insert(my_tree, 3);
+  insert(my_tree, 12);
+  insert(my_tree, 10);
+  insert(my_tree, 13);
+  binary_src(my_tree, 3);
+  binary_src(my_tree, 8);
+  print_pre_order(my_tree);
   
   return 0;
 }
