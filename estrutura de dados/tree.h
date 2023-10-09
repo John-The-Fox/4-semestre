3db1223ff5ef814faef6ee00f474efaff83ace8e
John the Fox <joao.chivite@gmail.com>
2023-10-09 19:54:57 -0300

    Update tree.h

--- estrutura de dados/tree.h
+++ estrutura de dados/tree.h
@@ -4,3 +4,5 @@ Tree* create_tree();
 void insert(Tree* tree,int value);
 int binary_src(Tree *tree, int value);
 void print_pre_order(Tree *tree);
+void print_in_order(Tree *tree);
+void print_pos_order(Tree *tree);
